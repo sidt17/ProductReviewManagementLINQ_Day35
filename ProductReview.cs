490b264aa256a1b17ec414d1f9a7723d5dec402b
Siddhant <santoshshrivastava39@gmail.com>
2022-04-18 17:31:21 +0530

    Model Class

---
+++ ProductReview.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagementWithLinq
+{
+    public class ProductReview
+    {
+        public int ProductID
+        {
+            get;
+            set;
+        }
+
+        public int UserID
+        {
+            get; 
+            set;
+        }
+
+        public int Rating
+        {
+            get; 
+            set;
+        }
+        public string Review
+        {
+            get; 
+            set;
+        }
+
+        public bool isLike
+        {
+            get;
+            set;
+        }
+    }
+    
+}
